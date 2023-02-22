46495af37363fd04036feab333f923e95c460ad5
KamyabT <dgghost034@gmail.com>
2023-02-22 14:33:51 +0330

    functionality added

--- src/Components/Temper.jsx
+++ src/Components/Temper.jsx
@@ -2,26 +2,37 @@ import React, { Component, useState } from "react";
 
 const Temper = () => {
   const [temperature, setTemperature] = useState(10);
+  const [temperaturecolor, setTemperaturecolor] = useState("cold");
+
+  const doIncrement = () => {
+    const newTemperature = temperature + 1;
+    if (newTemperature >= 15) {
+      setTemperaturecolor("hot");
+    }
+    setTemperature(newTemperature);
+  };
+
+  const deCrement = () => {
+    const newTemperature = temperature - 1;
+    if (newTemperature < 15) {
+      setTemperaturecolor("cold");
+    }
+    setTemperature(temperature - 1);
+  };
 
   return (
     <>
       <div className="mainDesign">
         <div className="container">
           <div className="appContainer">
-            <div className="tempHolder">
+            <div className={`tempHolder ${temperaturecolor}`}>
               <span className="temp">{temperature}°C</span>
             </div>
             <div>
-              <button
-                className="btnMainDesign"
-                onClick={() => setTemperature(temperature + 1)}
-              >
+              <button className="btnMainDesign" onClick={() => doIncrement()}>
                 +
               </button>
-              <button
-                className="btnMainDesign"
-                onClick={() => setTemperature(temperature - 1)}
-              >
+              <button className="btnMainDesign" onClick={() => deCrement()}>
                 -
               </button>
             </div>
